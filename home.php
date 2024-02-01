ee79bd352b232ca0d708cb5502e9e422e2f79bda
aSujith <sujith.ct22@bitsathy.ac.in>
2024-02-01 11:39:41 +0530

    Modified search-bar

--- home.php
+++ home.php
@@ -370,7 +370,7 @@
                     </div>
                     <div class="ps-3">
                       <h6>Attendence Count</h6>
-                      <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span>
+                      <span class="text-success small pt-1 fw-bold">0%</span> <span class="text-muted small pt-2 ps-1">increase</span>
 
                     </div>
                   </div>
